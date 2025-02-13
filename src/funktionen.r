f72031f303616637f23062ed4514cc4454394f2b
juleschuette <jule.schuette@tu-dortmund.de>
2025-02-13 20:28:40 +0100

    Update funktionen.r
    visualisierung_ueberlebensrate_klasse()

--- src/funktionen.r
+++ src/funktionen.r
@@ -9,5 +9,16 @@
 # R1iv metrische und dichotomen Variablen
 
 # R1v Visualisierung kategoriale Variablen
+## geeignete Visualisierung von drei oder vier kategorialen Variablen
+visualisierung_ueberlebensrate_klasse <- function(daten, x_var, fill_var, aufteilung) {
+  ggplot(daten, aes(x = x_var, fill = fill_var)) +
+    geom_bar() +
+    facet_wrap(~ aufteilung) +
+    labs(title = paste("Visualisierung von ", x_var, " nach ", fill_var, " und ", aufteilung),
+         x = x_var,
+         y = "Anzahl")
+}
+# Aufruf: visualisierung_ueberlebensrate_klasse(titanic, "Pclass", "Survived", "Sex")
+
 
 # R1vi weitere Funktionen
