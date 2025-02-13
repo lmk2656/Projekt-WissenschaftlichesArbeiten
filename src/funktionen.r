7b3e752f5b4870b083cf751fb6b11e147251910c
juleschuette <jule.schuette@tu-dortmund.de>
2025-02-13 20:09:29 +0100

    Create funktionen.r
    Grundgerüst

---
+++ src/funktionen.r
@@ -0,0 +1,13 @@
+# Funktionen für die Auswertung und Visualisierung
+
+# R1i Statistiken für metrische Variablen
+
+# R1ii Statistiken für kategoriale Variablen
+
+# R1iii Zusammenhang kategoriale Variablen
+
+# R1iv metrische und dichotomen Variablen
+
+# R1v Visualisierung kategoriale Variablen
+
+# R1vi weitere Funktionen
