22839ceb7753015c9d026b2df694bb97f882a4eb
Mareike-Uni <mareike.eichhorn@tu-dortmund.de>
2025-02-18 18:58:59 +0100

    Update funktionen.r

--- src/funktionen.r
+++ src/funktionen.r
@@ -1,6 +1,16 @@
 # Aufgabe 2: Funktionen für die Auswertung und Visualisierung
 
 # R1i Statistiken für metrische Variablen
+# Variablen muessen numerisch sein:
+str(titanic)
+is.numeric(titanic$Age) # TRUE
+is.numeric(titanic$SibSp) # TRUE
+is.numeric(titanic$Parch) # TRUE
+is.numeric(titanic$Fare) # TRUE
+## Alle anderen Variablen sind FALSE, also nicht numerisch und können nicht in diese Funktion eingesetzt werden.
+
+# Bei numerischen Variablen kann man den Mittelwert, den Median, die Varianz, die Standardabweichung, das Minimum, das Maximum 
+# und die Spannweite berechnen lassen:
 metrisch <- function(x){
     print(list(Mittelwert = mean(x, na.rm = TRUE),
                Median = median(x, na.rm = TRUE),
@@ -12,6 +22,17 @@ metrisch <- function(x){
 }
 
 # R1ii Statistiken für kategoriale Variablen
+## Variablen sollten als Klasse "factor" oder "character" haben:
+str(titanic)
+class(titanic$Survived) # factor
+class(titanic$Pclass) # ordered factor
+class(titanic$Sex) # factor
+class(titanic$Embarked) # factor
+class(titanic$Anrede) # character
+class(titanic$Bordseite) # character
+class(titanic$Deck) # character
+
+# Bei kategorialen Variablen kann man die absolute Haeufigkeit, die relative Haeufigkeit und den Modus berechnen lassen:
 kategorial <- function(y){
   print(list(absolute.Haeufigkeit = table(y),
              relative.Haeufigkeit = prop.table(table(y)),
